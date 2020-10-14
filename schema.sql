090443ae520af3e9f4210c2152ba6d7b77e5b874
Daniel Kang <dkang23@gmail.com>
2020-10-13 22:01:49 -0400

    schema sort of done

--- schema.sql
+++ schema.sql
@@ -1,21 +1,21 @@
 \connect questions;
 
-DROP TABLE IF EXISTS questions.prod_q;
+DROP TABLE IF EXISTS prod_q CASCADE;
 
-CREATE TABLE questions.prod_q (
+CREATE TABLE prod_q (
   id SERIAL,
   product_id INTEGER DEFAULT NULL,
   question_id INTEGER UNIQUE DEFAULT NULL,
   PRIMARY KEY (id, question_id)
 );
 
-CREATE INDEX prod_q on questions.prod_q (question_id)
+CREATE INDEX prod_q on prod_q (question_id);
 
-DROP TABLE IF EXISTS questions.questions;
+DROP TABLE IF EXISTS questions CASCADE;
 
-CREATE TABLE questions.questions (
+CREATE TABLE questions (
   id SERIAL,
-  question_id INTEGER DEFAULT NULL,
+  question_id INTEGER UNIQUE DEFAULT NULL,
   question_body TEXT DEFAULT NULL,
   question_date TIMESTAMP DEFAULT NULL,
   asker_name TEXT DEFAULT NULL,
@@ -23,54 +23,54 @@ CREATE TABLE questions.questions (
   reported INTEGER DEFAULT NULL,
   PRIMARY KEY (id, question_id),
   CONSTRAINT prod_question
-    FOREIGN KEY (question_id) REFERENCES questions.prod_q(question_id)
+    FOREIGN KEY (question_id) REFERENCES prod_q(question_id)
 );
 
-CREATE INDEX questions on questions.questions (question_id)
+CREATE INDEX questions on questions(question_id);
 
-DROP TABLE IF EXISTS questions.q_a;
+DROP TABLE IF EXISTS q_a CASCADE;
 
-CREATE TABLE questions.q_a (
+CREATE TABLE q_a (
   id SERIAL,
   question_id INTEGER DEFAULT NULL,
-  answer_id INTEGER DEFAULT NULL,
+  answer_id INTEGER UNIQUE DEFAULT NULL,
   PRIMARY KEY (id, answer_id),
   CONSTRAINT question_qa
-    FOREIGN KEY (question_id) REFERENCES questions.questions(question_id)
+    FOREIGN KEY (question_id) REFERENCES questions(question_id)
 );
 
-CREATE INDEX q_a on questions.q_a (question_id, answer_id)
+CREATE INDEX q_a on q_a (question_id, answer_id);
 
 
-DROP TABLE IF EXISTS questions.answers;
+DROP TABLE IF EXISTS answers CASCADE;
 
-CREATE TABLE questions.answers (
+CREATE TABLE answers (
   id SERIAL,
-  answer_id INTEGER NULL DEFAULT NULL,
-  answer_body TEXT NULL DEFAULT NULL,
-  answer_date TIMESTAMP NULL DEFAULT NULL,
-  answerer_name TEXT NULL DEFAULT NULL,
-  answer_helpfulness INTEGER NULL DEFAULT NULL,
+  answer_id INTEGER UNIQUE DEFAULT NULL,
+  answer_body TEXT DEFAULT NULL,
+  answer_date TIMESTAMP DEFAULT NULL,
+  answerer_name TEXT DEFAULT NULL,
+  answer_helpfulness INTEGER DEFAULT NULL,
   PRIMARY KEY (id,answer_id),
   CONSTRAINT qa_answer
-    FOREIGN KEY (answer_id) REFERENCES questions.q_a(answer_id)
+    FOREIGN KEY (answer_id) REFERENCES q_a(answer_id)
 );
 
-CREATE INDEX answers on questions.answers (answer_id)
+CREATE INDEX answers on answers (answer_id);
 
-DROP TABLE IF EXISTS questions.photos;
+DROP TABLE IF EXISTS photos CASCADE;
 
-CREATE TABLE questions.photos (
+CREATE TABLE photos (
   id SERIAL,
-  answer_id INTEGER NULL DEFAULT NULL,
-  photo_id INTEGER NULL DEFAULT NULL,
-  photo_url TEXT NULL DEFAULT NULL,
+  answer_id INTEGER DEFAULT NULL,
+  photo_id INTEGER DEFAULT NULL,
+  photo_url TEXT DEFAULT NULL,
   PRIMARY KEY (id, photo_id),
   CONSTRAINT ans_photo
-    FOREIGN KEY (answer_id) REFERENCES questions.answers(answer_id)
+    FOREIGN KEY (answer_id) REFERENCES answers(answer_id)
 );
 
-CREATE INDEX photos on questions.photos (answer_id)
+CREATE INDEX photos on photos (answer_id);
 
 
 -- Table Properties
