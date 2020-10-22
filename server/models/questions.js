c6d958888a1a8f32a7632e02bb3befdbdefe37dc
Daniel Kang <dkang23@gmail.com>
2020-10-22 19:50:22 -0400

    updated getAllQuestions query

--- server/models/questions.js
+++ server/models/questions.js
@@ -4,10 +4,8 @@ var getAllQuestions = (productId) => {
   return db
     .query(
       `select q.question_id,q.question_body, q.question_date,q.asker_name,q.question_helpfulness,q.question_reported, answers.*,photos.photo_id,photos.photo_url from (select * from questions where product_id = ${productId}) q
-    left join q_a
-    on q.question_id = q_a.question_id
     left join answers
-    on q_a.answer_id = answers.answer_id
+    on q.answer_id = answers.answer_id
     left join photos
     on answers.answer_id = photos.answer_id;`
     )
