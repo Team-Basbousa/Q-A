eca615f47a650bd29ae346676430f2fdcb80768e
Daniel Kang <dkang23@gmail.com>
2020-10-22 19:58:07 -0400

    updated query

--- server/models/questions.js
+++ server/models/questions.js
@@ -3,7 +3,7 @@ var db = require('../db');
 var getAllQuestions = (productId) => {
   return db
     .query(
-      `select q.question_id,q.question_body, q.question_date,q.asker_name,q.question_helpfulness,q.question_reported, answers.*,photos.photo_id,photos.photo_url from (select * from questions where product_id = ${productId}) q
+      `select q.question_id,q.answer_id,q.question_body, q.question_date,q.asker_name,q.question_helpfulness,q.question_reported, answers.*,photos.photo_id,photos.photo_url from (select * from questions where product_id = ${productId}) q
     left join answers
     on q.answer_id = answers.answer_id
     left join photos
