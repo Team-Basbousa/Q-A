7fdd060117d881f74af0e877430ba29fdb22e04a
Daniel Kang <dkang23@gmail.com>
2020-10-22 21:35:03 -0400

    changed query2

--- server/models/questions.js
+++ server/models/questions.js
@@ -6,7 +6,7 @@ var getAllQuestions = (productId) => {
       `select q.question_id,q.question_body, q.question_date,q.asker_name,q.question_helpfulness,q.question_reported, answers.*,photos.photo_id,photos.photo_url from
       questions as q
     left join answers
-    on q.question_id = answers.question_id  answers
+    on q.question_id = answers.question_id
     from left join photos
     on answers.answer_id = photos.answer_id
     where q.product_id = ${productId};`
