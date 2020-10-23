af1e09500637cd720972de4e58e7e6fcf2849f2d
Daniel Kang <dkang23@gmail.com>
2020-10-22 21:29:13 -0400

    changed query

--- server/models/questions.js
+++ server/models/questions.js
@@ -3,11 +3,13 @@ var db = require('../db');
 var getAllQuestions = (productId) => {
   return db
     .query(
-      `select (q.question_id,q.question_body, q.question_date,q.asker_name,q.question_helpfulness,q.question_reported, answers.*,photos.photo_id,photos.photo_url from (select * from questions where product_id = ${productId}) q
+      `select q.question_id,q.question_body, q.question_date,q.asker_name,q.question_helpfulness,q.question_reported, answers.*,photos.photo_id,photos.photo_url from
+      questions as q
     left join answers
-    on q.question_id = answers.question_id ) answers
-    left join photos
-    on answers.answer_id = photos.answer_id;`
+    on q.question_id = answers.question_id  answers
+    from left join photos
+    on answers.answer_id = photos.answer_id
+    where q.product_id = ${productId};`
     )
     .then((data) => {
       // console.log('gotAllQuestions//model');
