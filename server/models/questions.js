d7f9748f090c767af0342d43c586ace0aabbe652
Daniel Kang <dkang23@gmail.com>
2020-10-22 21:21:07 -0400

    different query

--- server/models/questions.js
+++ server/models/questions.js
@@ -3,9 +3,9 @@ var db = require('../db');
 var getAllQuestions = (productId) => {
   return db
     .query(
-      `select q.question_id,q.question_body, q.question_date,q.asker_name,q.question_helpfulness,q.question_reported, answers.*,photos.photo_id,photos.photo_url from (select * from questions where product_id = ${productId}) q
+      `select (q.question_id,q.question_body, q.question_date,q.asker_name,q.question_helpfulness,q.question_reported, answers.*,photos.photo_id,photos.photo_url from (select * from questions where product_id = ${productId}) q
     left join answers
-    on q.question_id = answers.question_id
+    on q.question_id = answers.question_id ) answers
     left join photos
     on answers.answer_id = photos.answer_id;`
     )
